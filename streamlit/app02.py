09302a98a7975a105ff5f27b32cebd36b950ba2a
Your Name <you@example.com>
2024-02-15 20:31:55 +0700

    first commit

---
+++ streamlit/app02.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.sidebar.text('Sidebar')
+st.sidebar.button('go>')
+st.sidebar.button('went>')
+st.sidebar.button('gone>')
+st.title('ลุงพล เต้นเต่างอย')
+st.image('https://tse1.mm.bing.net/th?id=OIP.kPdcnTKWDiLMNQymEhjA_gHaLH&pid=Api&P=0&h=180')
+st.text_input('What s your neme?')
